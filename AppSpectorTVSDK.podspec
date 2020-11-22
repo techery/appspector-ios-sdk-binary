ca55a2351eae019fb8f0890b5abcf083a8afaeb4
AppSpector CI <heroes@techery.io>
2020-11-22 20:26:50 +0200

    SDK update: v 1.3.7 (1.3.7)

--- AppSpectorTVSDK.podspec
+++ AppSpectorTVSDK.podspec
@@ -6,7 +6,9 @@ Pod::Spec.new do |s|
     s.license          = { type: 'MIT', file: 'LICENSE' }
     s.author           = { 'Techery' => 'heroes@techery.io' }
     s.platform         = :tvos
-    s.tvos.deployment_target = '9.0'
+    s.tvos.deployment_target = '10.0'
     s.tvos.vendored_frameworks = 'AppSpectorTVSDK.framework'
     s.source           = { :http => 'https://github.com/techery/appspector-ios-sdk-binary/blob/master/AppSpectorTVSDK.zip?raw=true' }
+    s.pod_target_xcconfig = { 'EXCLUDED_ARCHS[sdk=appletvsimulator*]' => 'arm64' }
+    s.user_target_xcconfig = { 'EXCLUDED_ARCHS[sdk=appletvsimulator*]' => 'arm64' }
 end
--- Package.swift
+++ Package.swift
@@ -9,26 +9,21 @@ let package = Package(
     products: [
         .library(
             name: "AppSpector",
-            targets: ["AppSpectorSDK", "AppSpectorSDKE2E", "AppSpectorTVSDK"])
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
+            targets: ["AppSpectorSDK"]),
+        .library(
+            name: "AppSpectorTVSDK",
+            targets: ["AppSpectorTVSDK"])
     ],
     targets: [
         .binaryTarget(
             name: "AppSpectorSDK",
             url: "https://github.com/techery/appspector-ios-sdk-binary/releases/download/1.3.7/AppSpectorSDK.xcframework.zip",
-            checksum: "d1997654da7d30b6e8f766b964a71f4d3374f7288357e85bdf3024695c5b6ea8"
-        ),
-        .binaryTarget(
-            name: "AppSpectorSDKE2E",
-            url: "https://github.com/techery/appspector-ios-sdk-binary/releases/download/1.3.7/AppSpectorSDKE2E.xcframework.zip",
-            checksum: '556c1acfc9c39b8090aa663f3111c556babaa0c2f457c204b2cd2869c42cf4e2'
+            checksum: "f2f0a5c54e1d9af4f7a52a0a9fa9d64eb18f35dd412e8eef1dc838b696a9f210"
         ),
         .binaryTarget(
             name: "AppSpectorTVSDK",
             url: "https://github.com/techery/appspector-ios-sdk-binary/releases/download/1.3.7/AppSpectorTVSDK.xcframework.zip",
-            checksum: "e84d645e92b7d8860d2dee14e27bafa8ee6998005a2d21d85f9afbe24cdb2ede"
+            checksum: "1e79b5e556c27349cdb8808bdb2849593dd8f7734032174a5c69475ef8b4ff47"
         )
     ]
 )
