e09de65a6fe1c362c54fa97a7ab356c9b695e6d0
AppSpector CI <heroes@techery.io>
2020-11-27 19:05:05 +0000

    SDK update: v 1.3.7 (1.3.7)

--- Package.swift
+++ Package.swift
@@ -18,12 +18,12 @@ let package = Package(
         .binaryTarget(
             name: "AppSpectorSDK",
             url: "https://github.com/techery/appspector-ios-sdk-binary/releases/download/1.3.7/AppSpectorSDK.xcframework.zip",
-            checksum: "d2b75294231a8148517b27f92a8989e7cb06a47bc2f11e5d9835182549a4cdec"
+            checksum: "f3f3379f4d8eba9659b8b0eb1c2e58de1f58a66f69787614fe10622552c943b4"
         ),
         .binaryTarget(
             name: "AppSpectorTVSDK",
             url: "https://github.com/techery/appspector-ios-sdk-binary/releases/download/1.3.7/AppSpectorTVSDK.xcframework.zip",
-            checksum: "59f54c3f1cc81fda518aa9b9f82bc33ab5f5d6fdc19625e5c304221b82585b46"
+            checksum: "a8fc97e918aa21eab017f6a6da16a8ef44a6727fae9a47fd01326f478a558a49"
         )
     ]
 )
