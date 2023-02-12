037c3db356793feda0d1bcbffeece9405e6b9d82
AppSpector CI <heroes@techery.io>
2023-02-12 21:46:36 +0000

    SDK update: v 1.4.8

--- Package.swift
+++ Package.swift
@@ -21,17 +21,17 @@ let package = Package(
         .binaryTarget(
             name: "AppSpectorSDK",
             url: "https://github.com/techery/appspector-ios-sdk-binary/releases/download/1.4.8/AppSpectorSDK.xcframework.zip",
-            checksum: "15d78c7db009dc1fc1b78a63133f12314b7169442de176accbf953488bbbeb79"
+            checksum: "33385e30286b5e99402f0b9b4a4f7495ee8f5c5144d370ba106f167391825cd9"
         ),
         .binaryTarget(
             name: "AppSpectorSDKE2E",
             url: "https://github.com/techery/appspector-ios-sdk-binary/releases/download/1.4.8/AppSpectorSDKE2E.xcframework.zip",
-            checksum: "f533cffed85e9a1bbf62ff4753edd2ee7e6626061578f5ff0aaf21ce8f708d46"
+            checksum: "277a5a594482c0545780447df7c67b5658d06974c3dc84bb3bf76451a50d2b4d"
         ),
         .binaryTarget(
             name: "AppSpectorTVSDK",
             url: "https://github.com/techery/appspector-ios-sdk-binary/releases/download/1.4.8/AppSpectorTVSDK.xcframework.zip",
-            checksum: "48bc1fb3aa73f6e685972ba303f0602ebb40f5987967a5a03cdbe7bc2812a87a"
+            checksum: "d8773d025716336cfef61b5e8228e59e53d6bc557299951d958c4da512b573ff"
         )
     ]
 )
