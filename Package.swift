ea900d980e7fac184fc1b18d178578749f582f0c
Deszip <raf.rafiki@gmail.com>
2023-08-10 20:14:48 +0300

    Updated links

--- Package.swift
+++ Package.swift
@@ -23,22 +23,22 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "AppSpectorSDK",
-            url: "https://raw.githubusercontent.com/techery/appspector-ios-sdk-binary/master/AppSpectorSDK.xcframework.zip",
+            url: "https://raw.githubusercontent.com/techery/appspector-ios-sdk-binary/master/AppSpectorSDK.xcframework",
             checksum: "3266d4c85f1794a23e09809d461236300495e796059f9c7703f04df24fa7410a"
         ),
         .binaryTarget(
             name: "AppSpectorSDKE2E",
-            url: "https://raw.githubusercontent.com/techery/appspector-ios-sdk-binary/master/AppSpectorSDKE2E.xcframework.zip",
+            url: "https://raw.githubusercontent.com/techery/appspector-ios-sdk-binary/master/AppSpectorSDKE2E.xcframework",
             checksum: "c8cc170cb78913cb964b7a23a0964e54851ad1c8ebccdf86784885c55de18774"
         ),
         .binaryTarget(
             name: "AppSpectorTVSDK",
-            url: "https://raw.githubusercontent.com/techery/appspector-ios-sdk-binary/master/AppSpectorTVSDK.xcframework.zip",
+            url: "https://raw.githubusercontent.com/techery/appspector-ios-sdk-binary/master/AppSpectorTVSDK.xcframework",
             checksum: "64314e57b562da1a6471aa5369144fdb1b5bce8f4cd4966065b858a60a1dbf22"
         )
         .binaryTarget(
             name: "AppSpectorOSXSDK",
-            url: "https://raw.githubusercontent.com/techery/appspector-ios-sdk-binary/master/AppSpectorOSXSDK.xcframework.zip",
+            url: "https://raw.githubusercontent.com/techery/appspector-ios-sdk-binary/master/AppSpectorOSXSDK.xcframework",
             checksum: "64314e57b562da1a6471aa5369144fdb1b5bce8f4cd4966065b858a60a1dbf22"
         )
     ]
